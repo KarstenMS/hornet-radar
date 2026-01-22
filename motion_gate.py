70cc27e8c9acb1292fa37feb12069128bd24e17c
KarstenKM <Karst.maus@gmail.com>
2026-01-22 17:54:56 +0100

    update

--- motion_gate.py
+++ motion_gate.py
@@ -264,7 +264,7 @@ class MotionGate:
         self.tracking_state.confirmed_frame = self.tracking_state.last_good_frame
         self.tracking_state.confirmed_frame_shape = self.tracking_state.last_good_frame_shape
 
-        #self.tracking_state.confirmed_bbox = best_det["bbox"]
+        self.tracking_state.confirmed_bbox = best_det["bbox"]
         self.tracking_state.stable_bbox = best_det["bbox"]
         self.tracking_state.confirmed_yolo_bbox = best_det["bbox"]  
         self.tracking_state.confirmed_centers = list(self.tracking_state.centers)
