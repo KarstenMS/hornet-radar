989f3b6bfbb1bc81bb9f42513098e25092b99087
KarstenKM <Karst.maus@gmail.com>
2026-02-06 18:25:52 +0100

    update

--- main.py
+++ main.py
@@ -143,7 +143,9 @@ def process_camera(motion_gate: MotionGate):
             if SHOW_DEBUG_VIDEO:
                 display = frame.copy()
                 draw_debug_overlay(display, debug)
-                cv2.namedWindow("Hornet Debug", cv2.WINDOW_AUTOSIZE)
+                cv2.namedWindow("Hornet Debug", cv2.WINDOW_NORMAL)
+                cv2.resizeWindow("Debug", CAMERA_WIDTH, CAMERA_HEIGHT)
+
                 #cv2.imshow("Hornet Debug", display)
 
                 if cv2.waitKey(1) & 0xFF == 27: 
